cf7edfdb5d294893762be14361e360d33fe51d3c
vadimelenev <gamerwow372@yandex.ru>
2024-12-07 04:52:43 +0300

    added appShopList

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,10 +1,21 @@
+//import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+//    alias(libs.plugins.androidLibrary)
 }
 
 kotlin {
-    
+//    androidTarget {
+//        compilations.all {
+//            compileTaskProvider.configure {
+//                compilerOptions {
+//                    jvmTarget.set(JvmTarget.JVM_1_8)
+//                }
+//            }
+//        }
+//    }
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -31,5 +42,24 @@ kotlin {
         iosMain.dependencies {
             implementation(libs.ktorClientIos)
         }
+        androidMain.dependencies {
+            implementation(libs.ktorClientAndroid)
+            implementation(libs.compose.ui)
+            implementation(libs.compose.ui.tooling.preview)
+            implementation(libs.compose.material3)
+            implementation(libs.androidx.activity.compose)
+        }
     }
 }
+
+//android {
+//    namespace = "com.yourapp.shoppinglist"
+//    compileSdk = 34
+//    defaultConfig {
+//        minSdk = 21
+//    }
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_1_8
+//        targetCompatibility = JavaVersion.VERSION_1_8
+//    }
+//}
